742ac6d68d99a1ce0a76abf4acdee478e04021a8
Alvaro Hernandez <alvarojhr96@gmail.com>
2021-09-23 20:00:01 -0500

    Calculating service

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -8,18 +8,31 @@ import Form from 'react-bootstrap/Form'
 import ListGroup from 'react-bootstrap/ListGroup'
 
 const Registro = () => {
+    const valorHora = 100
     const [value, setValue] = useState("");
     const [placas, setPlaca] = useState([])
 
-    const handleChange = (event) =>{
+    const handleChange = (event) => {
         setValue(event.target.value.toUpperCase());
     }
 
     const handleClick = () => {
+        
         if(value.length !== 7){
             alert("NO ES UNA PLACA DE CARRO")
         }else{
-            setPlaca([...placas,value])
+          const fecha = new Date(); 
+          const existe = placas.find((placa)=> placa.placa === value);
+
+          if(!existe){
+            setPlaca([...placas,{placa:value,horaEntrada:fecha}])
+          }
+          else{
+            existe.horaSalida = fecha
+            existe.precio = (parseInt((fecha - existe.horaEntrada)/60000)+1) * valorHora
+            setPlaca([...placas])
+          }
+            
         }
         console.log(placas)
     }
@@ -49,12 +62,27 @@ const Registro = () => {
             </Form>
             </Col>
           </Row>
-          <Row>
+          <Row className="d-flex justify-content-center align-items-center mt-3 mb-3">
+            <Col xs={6}>
           <ListGroup>
               {placas.map((placa) => {
-                  return (<ListGroup.Item>{placa}</ListGroup.Item>)
+                if(placa.horaSalida){
+                  return (
+                    <ListGroup.Item >
+                      {placa.placa} - {placa.horaEntrada.toLocaleString()} - {placa.horaSalida.toLocaleString()} - ${placa.precio}
+                    </ListGroup.Item>
+                    )
+                  }
+                  else{
+                    return (
+                      <ListGroup.Item >
+                        {placa.placa} - {placa.horaEntrada.toLocaleString()}
+                      </ListGroup.Item>
+                      )
+                    }
               })}
             </ListGroup>
+            </Col>
           </Row>
         </Container>
         
